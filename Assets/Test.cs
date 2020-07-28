5cfb136af4099c05dd7497696f5d7320d3588cad
masashitojo <masashi8646@yahoo.co.jp>
2020-07-28 22:09:41 +0900

    ScriptTestプロジェクト はじめてのコミット(課題提出1回目)
    スクリプトTest.csは課題(配列を宣言して出力しましょう)、Test_v2.csは発展課題(Bossクラスに、変数mpとMagic関数を作成してStart関数から呼び出しましょう)の内容です。

---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿/////////////////////////////////////
+///LESSON4 課題 配列を宣言して出力しましょう
+/////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 配列を初期化する
+		int[] points = {1, 2, 3, 4, 5};
+
+		// 配列の各要素の値を順番に表示
+		for (int i = 0; i < points.Length; i++) {
+			Debug.Log (points [i]);
+		}
+		// 配列の各要素の値を逆順に表示
+		for (int i = points.Length-1; i >= 0; i--) {
+			Debug.Log (points [i]);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Test_v2.cs
@@ -0,0 +1,61 @@
+﻿/////////////////////////////////////
+///LESSON4 課題 Bossクラスに、変数mpとMagic関数を作成してStart関数から呼び出しましょう
+/////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss {        
+	private int hp = 100;          // 体力
+	private int power = 25; // 攻撃力
+	private int mp = 53;      // 魔法
+
+	// 攻撃用の関数
+	public void Attack() { 
+		Debug.Log( this.power + "のダメージを与えた" );
+	}
+
+	// 防御用の関数
+	public  void Defence(int damage) { 
+		Debug.Log( damage+"のダメージを受けた" );
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
+
+	// 魔法攻撃用の関数
+	public void Magic() { 
+		if(this.mp>=5){
+			this.mp -= 5;
+			Debug.Log("魔法攻撃をした。残りMPは" + this.mp +"。");
+		} else {
+			Debug.Log("MPが足りないため魔法が使えない。");
+		}
+	}
+
+
+}
+
+public class Test_v2 : MonoBehaviour {
+
+	void Start () {
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+
+		// 攻撃用の関数を呼び出す
+		lastboss.Attack();
+		// 防御用の関数を呼び出す
+		lastboss.Defence(3);
+		// 魔法用の関数を10回使う
+		for(int i=0; i<10; i++){
+			lastboss.Magic();
+		}
+		// 魔法用の関数を1回使う(mpが足りないことを確認)
+		lastboss.Magic();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
